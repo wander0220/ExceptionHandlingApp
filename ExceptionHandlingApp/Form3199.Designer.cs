3c44df42ad5706c56b0d68f5056730713cf67251
Gihun Ham <progh2@gmail.com>
2020-09-22 10:16:15 +0900

    폼 내용 구현 - 숫자를 입력받아 100 곱해서 출력

--- ExceptionHandlingApp/Form3199.Designer.cs
+++ ExceptionHandlingApp/Form3199.Designer.cs
@@ -28,19 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(64, 43);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(130, 21);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "숫자를 넣어주세요!";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(44, 111);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(170, 49);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "숫자로 변환해서 100 곱하기";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(63, 73);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(130, 21);
+            this.txtResult.TabIndex = 3;
             // 
             // Form3199
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(289, 331);
+            this.ClientSize = new System.Drawing.Size(266, 179);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
             this.Name = "Form3199";
             this.Text = "Form3199";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtResult;
     }
 }
