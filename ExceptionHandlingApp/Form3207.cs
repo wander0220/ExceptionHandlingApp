0700052e967d3741db9a5ce0172956b24a5d4e57
소원 박 <s2018s29@e-mirim.hs.kr>
2020-09-22 14:47:17 +0900

    예외처리 적용

--- ExceptionHandlingApp/Form3207.cs
+++ ExceptionHandlingApp/Form3207.cs
@@ -24,10 +24,30 @@ namespace ExceptionHandlingApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            string input = txtinput.Text;
-            int result = int.Parse(input) * 100;
-            txtResult.Text = result.ToString();
+            try
+            {
+                string input = txtinput.Text;
+                int result = int.Parse(input) * 100;
+                txtResult.Text = result.ToString();
+                txtinput.Focus();
+            }
+            catch (FormatException exception)
+            {
+                MessageBox.Show("숫자를 입력해 주세요\n" + exception.Message);
+                txtinput.Text = "";
+                txtResult.Text = "";
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("알 수 없는 예외가 발생했습니다.\n" + exception.Message);
+                txtinput.Text = "";
+                txtResult.Text = "";
+            }
+            finally
+            {
+                txtinput.Focus();
+            }
+           
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
