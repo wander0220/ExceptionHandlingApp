a2e4e6de30c986d6b961e8b012df5e7158a0cfd4
주영 강 <jooyoung1025@outlook.kr>
2020-09-22 14:28:51 +0900

    예외 처리 폼 추가 및 버튼 폼 호출 연결

--- ExceptionHandlingApp/Form1.cs
+++ ExceptionHandlingApp/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -79,5 +80,11 @@ namespace ExceptionHandlingApp
             form.Show();        
         }
 
+        private void btn3201_Click(object sender, EventArgs e)
+        {
+            Form form = new Form3201();
+            form.Show();
+
+        }
     }
 }
---
+++ ExceptionHandlingApp/Form3201.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExceptionHandlingApp
+{
+    public partial class Form3201 : Form
+    {
+        public Form3201()
+        {
+            InitializeComponent();
+        }
+    }
+}
