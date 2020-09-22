0c357fb29c51eed6850ffae016194db31f373324
소원 박 <s2018s29@e-mirim.hs.kr>
2020-09-22 14:37:41 +0900

    숫자를 100배로 곱해주는 변환 코드 추가

--- ExceptionHandlingApp/Form3207.cs
+++ ExceptionHandlingApp/Form3207.cs
@@ -16,5 +16,23 @@ namespace ExceptionHandlingApp
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string input = txtinput.Text;
+            int result = int.Parse(input) * 100;
+            txtResult.Text = result.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
