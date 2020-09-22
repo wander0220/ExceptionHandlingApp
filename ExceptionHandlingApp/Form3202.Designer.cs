89df32a4fc4625c47a4af661fc1467c5cb50f490
Dohyun Kim <dohyunkim-git@users.noreply.github.com>
2020-09-22 14:37:54 +0900

    숫자를 100으로 곱해주는 변환코드 추가

--- ExceptionHandlingApp/Form3202.Designer.cs
+++ ExceptionHandlingApp/Form3202.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(12, 93);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(164, 104);
+            this.btnResult.TabIndex = 0;
+            this.btnResult.Text = "결과보기!";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 31);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(164, 25);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 62);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(164, 25);
+            this.txtResult.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("궁서체", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "숫자를 입력하세요";
+            // 
+            // Form3202
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(189, 204);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnResult);
+            this.Name = "Form3202";
             this.Text = "Form3202";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
     }
 }
