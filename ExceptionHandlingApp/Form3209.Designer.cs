d4738543d0761a760fb1ebaadbe821529262dee9
hwon22 <s2018s06@e-mirim.hs.kr>
2020-09-22 14:37:51 +0900

    숫자를 100로 뻥튀기해주는 코드 추가

--- ExceptionHandlingApp/Form3209.Designer.cs
+++ ExceptionHandlingApp/Form3209.Designer.cs
@@ -28,19 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnResult = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(68, 191);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 0;
+            this.btnResult.Text = "결과보기";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "숫자를 입력해주세요";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(68, 85);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 25);
+            this.txtInput.TabIndex = 2;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(68, 135);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 25);
+            this.txtResult.TabIndex = 3;
             // 
             // Form3209
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 467);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnResult);
             this.Name = "Form3209";
             this.Text = "Form3209";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
     }
 }
